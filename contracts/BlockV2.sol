4b050e7b27029ff94c1db55006978d5da4031b67
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-23 19:49:44 +0530

    complete box contract and proxy

---
+++ contracts/BlockV2.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+contract Box {
+    /* Storage variables */
+    uint256 internal s_vlaue;
+
+    /* Events */
+    event valueChanged(uint256 newValue);
+
+    /* Public functions */
+    function store(uint256 newValue) public {
+        s_vlaue = newValue;
+        emit valueChanged(newValue);
+    }
+
+    function retrive() public view returns (uint256) {
+        return s_vlaue;
+    }
+
+    function version() public pure returns (uint256) {
+        return 1;
+    }
+
+    function increment() public {
+        s_vlaue = s_vlaue + 1;
+        emit valueChanged(s_vlaue);
+    }
+}
